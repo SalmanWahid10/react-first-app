405a4c4eb90bda898341383ca14e753c6fcb5bd3
Salman Wahid <swchowdhury10@gmail.com>
2023-08-20 10:28:21 +0530

    init commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>Copyright ©2023 All rights reserved | SALMAN WAHID CHOWDHURY .</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://github.com/SalmanWahid10" target={"blank"}>
+            GitHub
+          </a>
+          <a
+            href="https://app.netlify.com/teams/swchowdhury10/sites"
+            target={"blank"}
+          >
+            Netlify
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
